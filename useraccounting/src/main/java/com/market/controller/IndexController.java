b127b590b26a8b78989e5c274da036b39c22c3aa
Johannes Weiss <johannesweiss_81@web.de>
2018-08-21 07:06:25 +0200

    Responsives design auf Startseite

--- useraccounting/src/main/java/com/market/controller/IndexController.java
+++ useraccounting/src/main/java/com/market/controller/IndexController.java
@@ -1,13 +1,6 @@
 package com.market.controller;
 
-import com.market.entities.UserEntity;
-import com.market.repositories.UserRepository;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -16,23 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
   
   /**
-   * Model.
-   */
-  @Autowired
-  private UserRepository userRepo;
-
-  /**
    * View.
    * 
    * @return the view
    */
-  @RequestMapping("/index")
-  public String index(ModelMap model) {
-
-    List<UserEntity> users = userRepo.findAll();
-    model.put("theUsers", users);
-
-    return "index";
+  @RequestMapping("/index2")
+  public String index() {
+    return "index2";
   }
   
 }
