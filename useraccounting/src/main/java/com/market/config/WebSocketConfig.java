7d6f74897b479dae62279600eb44c58605b48a9f
Johannes Weiss <johannesweiss_81@web.de>
2018-12-16 13:13:35 +0100

    WebSocketConfig

--- useraccounting/src/main/java/com/market/config/WebSocketConfig.java
+++ useraccounting/src/main/java/com/market/config/WebSocketConfig.java
@@ -1,5 +1,26 @@
 package com.market.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  /**
+   * This will be the endpoint the client connects to.
+   */
+  @Override
+  public void registerStompEndpoints(final StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws").withSockJS();
+  }
+
+  @Override
+  public void configureMessageBroker(final MessageBrokerRegistry registry) {
+    registry.setApplicationDestinationPrefixes("/app");
+    registry.enableSimpleBroker("/topic");
+  }
 }
