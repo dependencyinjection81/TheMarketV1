945670dbada983a7b56de414152b3af041cb68bf
Johannes Weiss <johannesweiss_81@web.de>
2018-12-19 09:09:03 +0100

    Working on LogoutSuccessHandler

--- useraccounting/src/main/java/com/market/controller/LogoutSuccessHandlerImpl.java
+++ useraccounting/src/main/java/com/market/controller/LogoutSuccessHandlerImpl.java
@@ -7,11 +7,18 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
+/**
+ * TODO SimpleUrlLogoutSuccessHandler implementieren da er noch nicht das event erfasst
+ * @author Johannes Weiß
+ *
+ */
 @Component("logoutSuccessHandlerImpl")
 public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
 
@@ -21,7 +28,12 @@ public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
   @Override
   public void onLogoutSuccess(final HttpServletRequest req, final HttpServletResponse res,
       final Authentication auth) throws IOException, ServletException {
-    System.out.println(auth.getName());
-    userService.setUserOffline(auth.getName());
+    HttpSession session = req.getSession();
+    if (session != null) {
+      session.removeAttribute("user");
+      System.out.println(auth.getName());
+      userService.setUserOffline(auth.getName());
+    }
+
   }
 }
